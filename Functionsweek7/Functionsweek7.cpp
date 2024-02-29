fe53797a666ed0773c11b0e560b5ec43ada2cb3d
ISHAN BANGA <ishan.banga0@gmail.com>
2024-02-29 16:25:48 -0500

    Addition of some Functions

--- Functionsweek7/Functionsweek7.cpp
+++ Functionsweek7/Functionsweek7.cpp
@@ -19,22 +19,31 @@ int Dividenumber(int a, int b) {
     int sum = a / b;
     return sum;
 }
+int max(int a, int b) {
+    if (a > b) 
+        return a;
+    else
+        return b;
+    }
+
 int main()
 {
     int a, b,i;
     std::cout << "Hello World!\n";
 
-    cout << "Enter First number:\t";
-    cin >> a;
-    cout << "Enter Second number:\t";
-    cin >> b;
-    cout << "Enter the following for their functions\n\n 1 to add numbers \n 2 to multiply numbers \n 3 to divide the numbers\n\n";
+    cout << "Enter Two Numbers number:\t";
+    cin >> a >> b;
+    //cout << "Enter Second number:\t";
+  //  cin >> b;
+    cout << "Enter the following for their functions\n\n 1 to add numbers \n 2 to multiply numbers \n 3 to divide the numbers\n 4 to see which number is bigger \n\n";
     cin >> i;
     switch (i) {
     case(1): {cout << "\nThe output is:" << addnumber(a, b) << endl;}
     case(2): {cout << "\nThe output is:" << Multiplynumber(a, b) << endl;}
     case(3): {cout << "\nThe output is:" << Dividenumber(a, b) << endl;}
-    default: { cout << "Enter a valid choice";}
+    case(4): {cout << "\nThe output is:" << max(a, b) << endl; }
+    default:
+    { cout << "Enter a valid choice";}
         break;
     }
     
